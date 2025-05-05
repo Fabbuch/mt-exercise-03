76b296ae2081ee4a71ff055a1ba88217b3ce8721
Fabio Bucher <fabio.bucher@uzh.ch>
2025-05-05 23:10:18 +0200

    added postnorm training log to plotting script arguments

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -5,4 +5,5 @@ base=$scripts/..
 
 python $scripts/plot.py \
     $base/models/deen_transformer_pre/baseline.log \
-    $base/models/deen_transformer_prenorm/train.log
+    $base/models/deen_transformer_prenorm/train.log \
+    $base/models/deen_transformer_postnorm/train.log
